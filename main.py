22b34ae8e08914dae66dac6f014e703b8937cc10
popik <165392782+dodik2353463@users.noreply.github.com>
2024-05-16 08:52:05 +0300

    Update main.py
    удален токен из общего доступа

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from handlers import include_router
 
-bot = Bot(token="7132743665:AAHmq7AFNRDRrWbG-Lz8vn9O2B8A84gmCY0")
+bot = Bot(token="")
 dp = Dispatcher()
 
 async def main():
